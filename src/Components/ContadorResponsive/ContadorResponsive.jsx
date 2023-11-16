47137d06a6edab9bc48318e27015e6edcf6f6d7b
FueguinoF <franciscoamador86@gmail.com>
2023-11-16 17:10:27 -0300

    Contador funcional - mejora en responsive - color mejorado - estilo con detalles  - faltan animaciones

--- src/Components/ContadorResponsive/ContadorResponsive.jsx
+++ src/Components/ContadorResponsive/ContadorResponsive.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Text } from '@chakra-ui/react';
+import { Button, Container, Grid, GridItem, Text } from '@chakra-ui/react';
 import {React ,  useState } from 'react';
 // import './ContadorResponsive.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -23,15 +23,22 @@ const ContadorResponsive=()=>{
 
     return(				
         <Container display={'flex'} flexDirection={'column'} alignItems={"center"} variant='elevated'
-            justifyContent={'space-around'} maxH={'500'} minH={'200'}>
+            justifyContent={'space-around'} maxH={'500'} minH={'200'} maxW={'400'}>
             <Container display={'flex'} flexDirection={'column'} alignItems={"center"} gap={'4'} fontFamily={'"seven segment" , sans-serif'}>
-                <Text fontSize={{base:'70', sm:'140', md:'200'}}>{contador}</Text>
+                <Text fontSize={{base:'90', sm:'140', md:'200'}}>{contador}</Text>
                 <Text fontSize={{base:'16', sm:'24', md:'32'}}> Número </Text>
             </Container>
-            <Container display={'flex'} flexDirection={{base:'column-reverse' , sm:'row'}} alignItems={"center"} justifyContent={'space-around'} minH={{base:'200px', sm:'100px'}}>
-                <Button onClick={restar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'red'} color={'red'} _hover={{bg:'rgb(236, 94, 69)', color:'white'}} isDisabled={contador <= 0}><FontAwesomeIcon icon={faSquareMinus} fontSize={'35'}  /></Button>
-                <Button onClick={borrar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'gray'} color={'gray'} _hover={{bg:'gray', color:'white'}}><FontAwesomeIcon icon={faTrashCan} beatFade fontSize={'30'} /></Button>
-                <Button onClick={sumar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'green'} color={'green'} _hover={{bg:'rgb(152, 236, 69)', color:'white'}}><FontAwesomeIcon icon={faSquarePlus} fontSize={'35'}/></Button>
+            {/* <Container display={'flex'} flexDirection={{base:'column-reverse' , sm:'row'}} alignItems={"center"} justifyContent={'space-around'} minH={{base:'200px', sm:'100px'}}>
+                <Button onClick={restar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'red'} color={'red'} _hover={{bg:'rgb(248, 134, 114)', color:'white'}} isDisabled={contador <= 0}><FontAwesomeIcon icon={faSquareMinus} fontSize={'35'}  /></Button>
+                <Button onClick={borrar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'gray'} color={'gray'} _hover={{bg:'rgb(184, 184, 184)', color:'white'}}><FontAwesomeIcon icon={faTrashCan} beatFade fontSize={'30'} /></Button>
+                <Button onClick={sumar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'green'} color={'green'} _hover={{bg:'rgb(125, 196, 55)', color:'white'}}><FontAwesomeIcon icon={faSquarePlus} fontSize={'35'}/></Button>
+            </Container> */}
+            <Container  minH={{base:'300px', sm:'100px'}}>
+                <Grid templateColumns={{base:'repeat(2,1fr)',md:'repeat(3,1fr)'}} rowGap={4} gap={4} alignItems={'center'}>
+                    <GridItem colSpan={1}><Button m={'2,2'} w={'full'} onClick={restar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'red'} color={'red'} _hover={{bg:'rgb(248, 134, 114)', color:'white'}} isDisabled={contador <= 0}><FontAwesomeIcon icon={faSquareMinus} fontSize={'35'}  /></Button></GridItem>
+                    <GridItem colSpan={{base:2 ,md:1}} order={{base:2 ,md:1}}><Button m={'2,2'} w={'full'}  onClick={borrar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'gray'} color={'gray'} _hover={{bg:'rgb(184, 184, 184)', color:'white'}}><FontAwesomeIcon icon={faTrashCan} beatFade fontSize={'30'} /></Button></GridItem>
+                    <GridItem colSpan={1} order={{base:1 ,md:2}}><Button m={'2,2'} w={'full'}  onClick={sumar} padding={'10px 30px'} borderRadius={'10px'} bg={'white'} outlineOffset={'2'} outlineColor={'green'} color={'green'} _hover={{bg:'rgb(125, 196, 55)', color:'white'}}><FontAwesomeIcon icon={faSquarePlus} fontSize={'35'}/></Button></GridItem>
+                </Grid> 
             </Container>
 
         </Container> 
